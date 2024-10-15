a1ed8633f4dbe906e1080fdcdedde3dd3c8a3e68
danial1221 <danialmujahid98@gmail.com>
2024-10-15 14:01:12 +0500

    Add files via upload

---
+++ Lab01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Numbers
+{
+    class program
+    {
+        static void main(string[] args)
+        {
+            ExploringNumbers();
+        }
+        private static void ExploringNumbers()
+        {
+            Console.WriteLine($"int uses{sizeof(int)} bytes and can store numbers in the range :\n\n" + $" {int.MinValue} to  {int.MaxValue: N0}.\n");
+
+            Console.WriteLine("___________________________________________________________________________________________________________________");
+
+            Console.WriteLine($"int uses{sizeof(double)} bytes and can store numbers in the range :\n\n" + $" {double.MinValue} to  {double.MaxValue: N0}.\n");
+            Console.WriteLine("___________________________________________________________________________________________________________________");
+
+            Console.WriteLine($"int uses{sizeof(decimal)} bytes and can store numbers in the range :\n\n" + $" {decimal.MinValue:N0} to  {decimal.MaxValue: N0}.\n");
+        }
+    }
+}
